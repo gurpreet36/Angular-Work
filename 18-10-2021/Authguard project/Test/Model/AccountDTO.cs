62cc9e379d763813424f2323b35610c3b774f65c
Gurpreet Singh <57633822+gurpreet36@users.noreply.github.com>
2021-10-18 17:28:24 +0530

    Delete 18-10-2021 directory

--- 18-10-2021/Authguard project/Test/Model/AccountDTO.cs
+++
@@ -1,9 +0,0 @@
-namespace Test.Model
-{
-    public class AccountDTO
-    {
-        public string Username { get; set; }
-        public string Password { get; set; }
-        public int Age { get; set; }
-    }
-}
